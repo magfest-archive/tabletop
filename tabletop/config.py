1d4bf7f5ae5751d76c7d87ef6fb9071e0eae877a
Dominic Cerquetti <binary1230@gmail.com>
2016-05-11 11:20:40 -0400

    gracefully skip tabletop rooms that aren't event_location

--- tabletop/config.py
+++ tabletop/config.py
@@ -3,4 +3,9 @@ from tabletop import *
 tabletop_config = parse_config(__file__)
 c.include_plugin_config(tabletop_config)
 
-c.TABLETOP_LOCATIONS = [getattr(c, loc.upper()) for loc in c.TABLETOP_LOCATIONS]
+invalid_tabletop_rooms = [room for room in c.TABLETOP_LOCATIONS if not getattr(c, room.upper(), None)]
+for room in invalid_tabletop_rooms:
+    log.warning('tabletop plugin: tabletop_locations config problem: '
+                'Ignoring {!r} because it was not also found in [[event_location]] section.'.format(room.upper()))
+
+c.TABLETOP_LOCATIONS = [getattr(c, room.upper()) for room in c.TABLETOP_LOCATIONS if room not in invalid_tabletop_rooms]
