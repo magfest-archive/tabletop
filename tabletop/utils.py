53d8a7622c74d39481d5d3664537b5583cfae65f
Rob Ruana <rob@robruana.com>
2018-02-08 17:16:50 -0500

    Renames tabletop config variables in preperation to merge tabletop into uber

--- tabletop/utils.py
+++ tabletop/utils.py
@@ -2,27 +2,34 @@ from tabletop import *
 
 twilio_client = None
 try:
-    twilio_sid = tabletop_config['secret']['twilio_sid']
-    twilio_token = tabletop_config['secret']['twilio_token']
+    twilio_sid = tabletop_config['secret']['tabletop_twilio_sid']
+    twilio_token = tabletop_config['secret']['tabletop_twilio_token']
 
     if twilio_sid and twilio_token:
         twilio_client = TwilioRestClient(twilio_sid, twilio_token)
     else:
-        log.debug('Twilio SID and/or TOKEN is not in INI, not going to try to start Twilio for SMS messaging')
-except:
+        log.debug(
+            'Tabletop twilio SID and/or TOKEN is not in INI, not going to try '
+            'to start twilio for tabletop SMS messaging')
+except Exception:
     log.error('twilio: unable to initialize twilio REST client', exc_info=True)
     twilio_client = None
 
 
 def normalize(phone_number):
-    return phonenumbers.format_number(phonenumbers.parse(phone_number, c.PHONE_COUNTRY), PhoneNumberFormat.E164)
+    return phonenumbers.format_number(
+        phonenumbers.parse(phone_number, c.TABLETOP_PHONE_COUNTRY),
+        PhoneNumberFormat.E164)
 
 
-def send_sms(to, body, from_=c.TWILIO_NUMBER):
+def send_sms(to, body, from_=c.TABLETOP_TWILIO_NUMBER):
     to = normalize(to)
     if not twilio_client:
         log.error('no twilio client configured')
     elif c.DEV_BOX and to not in c.TESTING_PHONE_NUMBERS:
-        log.info('We are in dev box mode, so we are not sending {!r} to {!r}', body, to)
+        log.info(
+            'We are in dev box mode, so we are not sending {!r} to {!r}',
+            body, to)
     else:
-        return twilio_client.messages.create(to=to, body=body, from_=normalize(from_))
+        return twilio_client.messages.create(
+            to=to, body=body, from_=normalize(from_))
